52e1cb2b68aeb8649b79d852621ca47b9926570a
jramke <jstrmk@gmail.com>
2024-09-20 17:15:36 +0200

    [FIX] Add `page.tsconfig` manually for v11

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,11 @@ defined('TYPO3') or die();
 // Define TypoScript as content rendering template.
 // This is normally set in Fluid Styled Content.
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'tailwindstyledcontent/Configuration/TypoScript/';
+
+// Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+$versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+if ($versionInformation->getMajorVersion() < 12) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        @import "EXT:tailwind_styled_content/Configuration/page.tsconfig"
+    ');
+}
